b080e9a777eef22883b5c836f04aa49365a87fc4
immersommer <ziyifu970225@gmail.com>
2022-05-09 15:47:04 -0700

    example-fastcall

---
+++ tools/testing/selftests/fastcall/fastcall.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/fcntl.h> 
+#include <sys/stat.h>
+#include <sys/ioctl.h>      
+#include <unistd.h>     
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define PAGE_SIZE sysconf(_SC_PAGE_SIZE)
+
+struct mesg {
+		size_t size;
+		void *address;
+};
+/*
+ * Read the first byte of the fastcall table.
+ */
+void _read_table(void *address)
+{
+	printf("First byte of the fastcall table: 0x%x\n", *(char *)address);
+}
+
+
+/*
+ * The fastcall table must not be unmapped.
+ * munmap should result in an error.
+ */
+int main(int argc, char *argv[])
+{
+    int fd;
+	int ret;
+
+	printf("enter\n");
+
+	unsigned long address = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_LOCKED, -1, 0);
+
+	assert(MAP_FAILED != address);
+
+	struct mesg msg1 = {.size = PAGE_SIZE, .address = (unsigned long)address};
+
+	_read_table(address);
+
+	fd = open("/dev/fastcall-examples", O_RDONLY);
+	if (fd < 0) {
+		printf("Cannot open device file...\n");
+	}
+	ret = ioctl(fd, 0, &msg1);
+
+	assert(ret == 0);
+	_read_table(address);
+}
